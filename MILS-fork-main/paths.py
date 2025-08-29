40d5cf53699a338b041ea4344a77c8d2f1deafce
Ben Chen <73368910+benzionchen@users.noreply.github.com>
2025-08-29 12:01:16 -0700

    updated paths

--- MILS-fork-main/paths.py
+++ MILS-fork-main/paths.py
@@ -4,21 +4,27 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+# Base data folder
+DATASET_DIR = "/Users/raiu/Desktop/Desktop/projects/data/MILS"
+
 # Output dir
-OUTPUT_DIR = '/path/to/output_dir/'
+OUTPUT_DIR = f"{DATASET_DIR}/outputs"
 
 # Image captioning
-IMAGEC_COCO_ANNOTATIONS = '/path/to/coco/val2014/annotations/captions_val2014.json'
-IMAGEC_COCO_IMAGES = '/path/to/coco/val2014/val2014'
-IMAGEC_COCO_SPLITS = '/path/to/coco/test/split' # TODO: add some reference here
+IMAGEC_COCO_ANNOTATIONS = f"{DATASET_DIR}/coco/annotations/captions_val2014.json"
+IMAGEC_COCO_IMAGES = f"{DATASET_DIR}/coco/val2014"
+IMAGEC_COCO_SPLITS = f"{DATASET_DIR}/coco/test/split" # TODO: add some reference here
 
 # Video captioning
-VIDEOC_MSRVTT_ANNOTATIONS = '/path/to/msrvtt/test_videodatainfo.json'
-VIDEOC_MSRVTT_VIDEOS = '/path/to/msrvtt/TestVideo/'
+VIDEOC_MSRVTT_ANNOTATIONS = f"{DATASET_DIR}/msrvtt/test_videodatainfo.json"
+VIDEOC_MSRVTT_VIDEOS = f"{DATASET_DIR}/msrvtt/TestVideo"
 
 # Audio captioning
-AUDIOC_CLOTHO_ANNOTATIONS = '/path/to/Clotho2/clotho_captions_evaluation.csv'
-AUDIOC_CLOTHO_FILES = '/path/to/Clotho2/wav/'
+AUDIOC_CLOTHO_ANNOTATIONS = f"{DATASET_DIR}/clotho/clotho_captions_evaluation.csv"
+AUDIOC_CLOTHO_FILES = f"{DATASET_DIR}/clotho/wav"
+
+# Checkpoints
+VICLIP_CKPT = f"{DATASET_DIR}/checkpoints/ViClip-InternVid-10M-FLT.pth"
 
 # Image generation
 
